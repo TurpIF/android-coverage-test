1b1dadff46523febb0e4688147f531b40feae3e5
p.turpin <p.turpin@criteo.com>
2020-08-06 20:31:26 +0200

    Add initial Gradle files

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+buildscript {
+    repositories {
+        google()
+        jcenter()
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:3.6.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.72")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        jcenter()
+    }
+}
