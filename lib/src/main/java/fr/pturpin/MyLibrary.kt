b18243313833e1980eb409c64e303be31129cab3
p.turpin <p.turpin@criteo.com>
2020-08-13 09:28:19 +0200

    Add dummy content to show the effect of the coverage report in PR
    When doing a new PR, it is expected that the Sonar bot will comment and
    shows the coverage report on the new code of the PR.
    On 3 methods, 2 are covered by tests (one by Java tests, one by Android
    tests), then we can expect the coverage ratio on the new code to be
    approximately 66%.

--- lib/src/main/java/fr/pturpin/MyLibrary.kt
+++ lib/src/main/java/fr/pturpin/MyLibrary.kt
@@ -4,11 +4,19 @@ class MyLibrary {
 
     // This is covered by JVM tests in the same module
     fun zero(): Int {
+        println("test")
         return 0
     }
 
     // This is covered by Instrumented tests in an other module
     fun one(): Int {
+        println("test")
         return 1
+    }
+
+    // This is not covered by any tests
+    @Suppress("unused")
+    fun two(): Int {
+        return 2
     }
 }
