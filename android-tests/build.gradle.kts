02f76bba60e64aa8203581f0e9ed82566c6ffe36
p.turpin <p.turpin@criteo.com>
2020-08-13 11:32:45 +0200

    Generate XML and HTML coverage reports when running android tests
    The Jacoco task from the Android Gradle plugin is completely custom. It
    generates automatically reports but it does not support cross-modules
    coverage.
    As the android tests are in a dedicated module, by default, the Android
    Jacoco will not gather the coverage of the lib module. The workaround is
    to make the sources of the lib belong to the sources of the test module.
    This mess up the IDE, so this workaround is only applied when Gradle is
    invoked with a dedicated parameter.

--- android-tests/build.gradle.kts
+++ android-tests/build.gradle.kts
@@ -10,10 +10,37 @@ android {
         minSdkVersion(29)
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+
+    if (isGatheringCodeCoverage()) {
+        buildTypes["debug"].isTestCoverageEnabled = true
+        sourceSets["debug"].addDebugSourceSetsOf(project(":lib"))
+    }
 }
 
 dependencies {
-    implementation(project(":lib"))
+    implementation(project(":lib")) // Still needed when Gradle is not gathering coverage
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.3.72")
     androidTestImplementation("androidx.test:runner:1.2.0")
+}
+
+/**
+ * When executing Android tests on debug, we want to gather the code coverage.
+ * The sources of the tested library are in another module and the Android Jacoco does not seems
+ * to handle cross-modules coverage.
+ *
+ * To handle this, the sources of the tested library are added in the classpath of the testing
+ * module. However, this make the IDE unstable and it is not possible to easily develop in such
+ * configuration. So classpath of testing module is extended only if a gradle property is defined.
+ */
+fun Project.isGatheringCodeCoverage(): Boolean {
+    return properties["codeCoverage"] == "true"
+}
+
+fun com.android.build.gradle.api.AndroidSourceSet.addDebugSourceSetsOf(project: Project) {
+    // Source sets should be defined during the configuration phase of Gradle.
+    // So at this time, the other project is not necessary fully configured, and the android
+    // extension might not be present yet.
+    // Hence we have to hard-code the paths to the debug source sets of the other project.
+    java.srcDir(project.file("src/main/java"))
+    java.srcDir(project.file("src/debug/java"))
 }
